7d58ee0f044cb1b0db3b441704b637e0171e96a4
07131031 <1051040839@qq.com>
2014-12-25 13:43:55 +0800

    开课吧作业13

---
+++ src/cn/edu/gdmec/s07131031/demo_gson/MainActivity.java
@@ -0,0 +1,60 @@
+package cn.edu.gdmec.s07131031.demo_gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        List<String> phones=new ArrayList<String>();
+        phones.add("123456");
+        phones.add("234567");
+        phones.add("3456789");
+        
+        Person person = new Person("zhangsan",20,phones);
+        
+        Gson gson = new Gson();
+        
+        String s=gson.toJson(person);
+        
+        Log.i("info", s);
+        
+        Person person2=gson.fromJson(s,Person.class );
+        
+        Log.i("info", "通过GSON生成java对象"+person2);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
